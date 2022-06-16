7e5a1bc297e75a8f607044e8cfdfcc20f536462c
Cindy Le <cindyledev@gmail.com>
2022-06-16 18:55:27 -0400

    delete gallery code to unbreak prod

--- pages/gallery.js
+++ pages/gallery.js
@@ -42,46 +42,9 @@ export default function Gallery({ initial_images, next_cursor }) {
             GALLERY
           </h2>
         </div>
-        <div className="grid grid-cols-1 mt-16 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8">
-          {initial_images.map((image) => (
-            <a key={image.id} href={image.image} className="group text-sm">
-              <div className="w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden bg-gray-100 group-hover:opacity-75">
-                <img
-                  src={image.image}
-                  alt={image.title}
-                  className="w-full h-full object-center object-cover"
-                />
-              </div>
-              <h3 className="mt-4 font-medium text-gray-900">{image.title}</h3>
-              <p className="text-gray-500 italic">{image.id}</p>
-            </a>
-          ))}
-        </div>
-        <div className="flex mt-4 mx-auto justify-center">
-          <button
-            onClick={handleLoadMore}
-            type="button"
-            className="inline-flex items-center mt-2 px-2.5 py-1.5 border border-transparent text-sm font-medium rounded shadow-sm text-white bg-red-800 hover:bg-red-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600"
-          >
-            LOAD MORE
-          </button>
-        </div>
+
       </div>
     </div>
   );
 }
 
-export async function getStaticProps() {
-  const results = await search();
-
-  const { resources, next_cursor } = results;
-
-  const initial_images = mapImageResources(resources);
-
-  return {
-    props: {
-      initial_images,
-      next_cursor,
-    },
-  };
-}

4ea786a6770269e691d072194ff5a547b5ea1728
Cindy Le <cindyledev@gmail.com>
2022-06-16 12:57:46 -0400

    use serverless function api

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,8 +1,7 @@
 import { search } from '../../lib/cloudinary';
 
 export default async function handler(req, res) {
-  const params = JSON.parse(req.body);
-  const results = await search(params);
+  const results = await search();
 
   res.status(200).json({ ...results });
 }
--- pages/gallery.js
+++ pages/gallery.js
@@ -1,7 +1,19 @@
+import { useEffect } from 'react';
 import Head from 'next/head';
 
-export default function Gallery({ images }) {
-  console.log(images);
+import { mapImageResources, search } from '../lib/cloudinary';
+
+export default function Gallery({ images, next_cursor }) {
+  console.log('images', images);
+  console.log('next_cursor', next_cursor);
+
+  useEffect(() => {
+    (async function run() {
+      const results = await fetch('/api/search').then((res) => res.json());
+      console.log('results', results);
+    })();
+  }, []);
+
   return (
     <div className="bg-white">
       <Head>
@@ -9,19 +21,17 @@ export default function Gallery({ images }) {
         <meta name="description" content="Pam and Frank's Wedding" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            GALLERY
-          </h2>
+      <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">GALLERY</h2>
       <div className="max-w-7xl mx-auto py-16 px-4 overflow-hidden sm:py-24 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8">
           {images.map((image) => (
             <a key={image.id} href={image.image} className="group text-sm">
               <div className="w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden bg-gray-100 group-hover:opacity-75">
-                <img
+                {/* <img
                   src={image.image}
                   alt={image.title}
                   className="w-full h-full object-center object-cover"
-                />
+                /> */}
               </div>
               <h3 className="mt-4 font-medium text-gray-900">{image.title}</h3>
               <p className="text-gray-500 italic">{image.id}</p>
@@ -34,32 +44,16 @@ export default function Gallery({ images }) {
 }
 
 export async function getStaticProps() {
-  const results = await fetch(
-    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`,
-    {
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          process.env.CLOUDINARY_API_KEY + ':' + process.env.CLOUDINARY_API_SECRET
-        ).toString('base64')}`,
-      },
-    }
-  ).then((res) => res.json());
+  const results = await search();
 
-  const { resources } = results;
+  const { resources, next_cursor } = results;
 
-  const images = resources.map((resource) => {
-    return {
-      id: resource.asset_id,
-      title: resource.public_id,
-      image: resource.secure_url,
-      width: resource.width,
-      height: resource.height,
-    };
-  });
+  const images = mapImageResources(resources);
 
   return {
     props: {
       images,
+      next_cursor,
     },
   };
 }

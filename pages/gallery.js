501fc4a8ace7ff431cb3b0a08c91a464b6261de2
Cindy Le <cindyledev@gmail.com>
2022-06-17 12:08:22 -0400

    fix next cursor

--- pages/gallery.js
+++ pages/gallery.js
@@ -3,18 +3,18 @@ import Head from 'next/head';
 
 import { mapImageResources, search } from '../lib/cloudinary';
 
-export default function Gallery({ initial_images, next_cursor }) {
-  const [images, setImages] = useState(initial_images);
-  const [nextCursor, setNextCursor] = useState(next_cursor);
-  console.log('initial images', initial_images);
-  console.log('next_cursor', next_cursor);
+export default function Gallery({ images: defaultImages, nextCursor: defaultNextCursor }) {
+  const [images, setImages] = useState(defaultImages);
+  const [nextCursor, setNextCursor] = useState(defaultNextCursor);
+  console.log('defaultImages', images);
+  console.log('nextCursor', nextCursor);
 
   async function handleLoadMore(event) {
     event.preventDefault();
 
     const results = await fetch('/api/search', {
       method: 'POST',
-      body: JSON.stringify({ next_cursor }),
+      body: JSON.stringify({ nextCursor }),
     }).then((res) => res.json());
 
     const { resources, next_cursor: updatedNextCursor } = results;
@@ -42,9 +42,46 @@ export default function Gallery({ initial_images, next_cursor }) {
             GALLERY
           </h2>
         </div>
-
+        <div className="grid grid-cols-1 mt-16 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8">
+          {images.map((image) => (
+            <a key={image.id} href={image.image} className="group text-sm">
+              <div className="w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden bg-gray-100 group-hover:opacity-75">
+                {/* <img
+                  src={image.image}
+                  alt={image.title}
+                  className="w-full h-full object-center object-cover"
+                /> */}
+              </div>
+              <h3 className="mt-4 font-medium text-gray-900">{image.title}</h3>
+              <p className="text-gray-500 italic">{image.id}</p>
+            </a>
+          ))}
+        </div>
+        <div className="flex mt-4 mx-auto justify-center">
+          <button
+            onClick={handleLoadMore}
+            type="button"
+            className="inline-flex items-center mt-2 px-2.5 py-1.5 border border-transparent text-sm font-medium rounded shadow-sm text-white bg-red-800 hover:bg-red-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600"
+          >
+            LOAD MORE
+          </button>
+        </div>
       </div>
     </div>
   );
 }
 
+export async function getStaticProps() {
+  const results = await search();
+
+  const { resources, next_cursor: nextCursor } = results;
+
+  const images = mapImageResources(resources);
+
+  return {
+    props: {
+      images,
+      nextCursor,
+    },
+  };
+}
